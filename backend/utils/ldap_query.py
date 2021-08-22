9f69bccc71aaa48b53d929138c252ed25b7209e1
Kishore <kishash22@gmail.com>
2021-08-22 12:34:34 +0300

    Added ldap query script

---
+++ backend/utils/ldap_query.py
@@ -0,0 +1,29 @@
+import ldap
+import re
+
+def get_ldap_data(email):
+
+	# Init connection with iiit's LDAP
+	l = ldap.initialize('ldaps://ldap.iiit.ac.in')
+
+	# Query database
+	results = l.search_s("ou=Users,dc=iiit,dc=ac,dc=in", ldap.SCOPE_SUBTREE, filterstr="(mail={})".format(email))
+
+	ret = {} # Return dictionary
+	
+	# Handle errors here
+	if(len(results)!=1):
+		raise ValueError("Unique match for given rollnumber not found in LDAP")
+
+	else:
+		res = results[0][1] 
+		# Add any other details if required to below dict
+		required = {'name':'cn', 'email':'mail', 'rollno':'uidNumber', 'gender':'gender'}
+		for req in required:
+			ret[req] = res[required[req]][0]
+		# Retrieve programme and course specially from the other string
+		infostr = results[0][0]
+		batch_details = list(re.split("uid=|,ou=|,dc=", infostr)[2:4])
+		ret['programme'] = batch_details[0]
+		ret['batch'] = batch_details[1]
+	return ret
