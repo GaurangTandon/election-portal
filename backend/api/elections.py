7be1bbc219ee0a4f8fb314a7afc2abef14cb8088
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2021-08-30 12:58:56 +0530

    ran Black

--- backend/api/elections.py
+++ backend/api/elections.py
@@ -27,9 +27,9 @@ parser.add_argument(
     "election_method",
     help="Method of election",
     type=str,
-    choices = ('STV', 'IRV'),
+    choices=("STV", "IRV"),
     location="form",
-    required=True
+    required=True,
 )
 parser.add_argument(
     "nomination_start_date",
@@ -78,7 +78,7 @@ class ElectionCreate(Resource):
         election = Election(
             title=args["name"],
             description=args["description"],
-            election_method = args["election_method"],
+            election_method=args["election_method"],
             nomination_start_date=args["nomination_start_date"],
             nomination_end_date=args["nomination_end_date"],
             voting_start_date=args["voting_start_date"],
