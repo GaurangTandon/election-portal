0b8ecd7724b3bb204ed32607250ad2497f6f283c
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-08-30 11:15:38 +0530

    Ran black on the Python files

--- backend/views/elections.py
+++ backend/views/elections.py
@@ -8,26 +8,40 @@ from backend.models.models import Candidates, Election
 
 election_routes = Blueprint("elections", __name__)
 
+
 @election_routes.route("/")
 # @auth_required
-def home():    
+def home():
     return render_template(
         "index.html",
         ongoing_election_list=Election.query.filter(
             Election.nomination_start_date < datetime.datetime.now()
         ).all(),
-        upcoming_election_list = Election.query.filter(Election.nomination_start_date > datetime.datetime.now()).all(),
-        now=datetime.datetime.now(), 
+        upcoming_election_list=Election.query.filter(
+            Election.nomination_start_date > datetime.datetime.now()
+        ).all(),
+        now=datetime.datetime.now(),
     )
+
 
 @election_routes.route("/<int:election_id>")
 def election_info(election_id):
     election = Election.query.get_or_404(election_id)
     candidates = list(election.candidates)
     random.shuffle(candidates)
-    return render_template("election/election.html", election=Election.query.get(election_id), candidates=candidates)
+    return render_template(
+        "election/election.html",
+        election=Election.query.get(election_id),
+        candidates=candidates,
+    )
+
 
 @election_routes.route("/<int:election_id>/candidate/<int:user_id>")
 def candidate_info(election_id, user_id):
     election = Election.query.get_or_404(election_id)
-    return render_template("election/candidate.html",election=election,candidate=election.get_candidate(user_id),owner=True)
+    return render_template(
+        "election/candidate.html",
+        election=election,
+        candidate=election.get_candidate(user_id),
+        owner=True,
+    )
