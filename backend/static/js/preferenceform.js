aa1ccf70db328f9c08075b6ed54f107c7d0ce0a1
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-08-31 19:58:45 +0530

    [broken code] JavaScript file refactoring

--- backend/static/js/preferenceform.js
+++ backend/static/js/preferenceform.js
@@ -9,73 +9,93 @@ const submitButton = document.getElementById('submitButton');
 let preferences = [];
 
 function clearall() {
-  form.reset();
-  error.style.display = 'none';
-  window.localStorage.setItem('preferences', JSON.stringify([]));
+    form.reset();
+    error.classList.remove(SHOW_CLS);
+    error.style.display = 'none';
+    window.localStorage.setItem('preferences', JSON.stringify([]));
 }
 
+/**
+ * @param {Element} x 
+ */
 function check(x) {
-  for (let i = 0; i < count; i += 1) {
-    if (form[i].value === x.value && form[i] !== x) {
-      form[i].value = '';
+    for (let i = 0; i < count; i += 1) {
+        if (form[i].value === x.value && form[i] !== x) {
+            form[i].value = '';
+        }
     }
-  }
-  for (let i = 0; i < count; i += 1) {
-    preferences[i] = form[i].value;
-  }
-  window.localStorage.setItem('preferences', JSON.stringify(preferences));
+    for (let i = 0; i < count; i += 1) {
+        preferences[i] = form[i].value;
+    }
+    window.localStorage.setItem('preferences', JSON.stringify(preferences));
 }
+
+const SHOW_CLS = "show";
 
 function submit() {
-  error.style.display = 'none';
-  checkboxError.style.display = 'none';
-  for (let i = 0; i < count; i += 1) {
-    preferences[i] = form[i].value;
-  }
-  // console.log(preferences)
-
-  if (preferences[0] === '') {
-    // console.log('empty');
-    error.style.display = 'block';
-    return false;
-  }
-
-  for (let i = 0; i < count; i += 1) {
-    if (preferences[i] === '') {
-      for (let j = i + 1; j < count; j += 1) {
-        if (preferences[j] !== '') {
-          // console.log('wrong order');
-          error.style.display = 'block';
-          return false;
-        }
-      }
-      // return preferences
-      // console.log(preferences);
-      window.localStorage.setItem('preferences', JSON.stringify(preferences));
-      if (checkbox.checked === true) {
-        submitButton.href = '../html/index.html';
-        return true;
-      }
-      checkboxError.style.display = 'block';
+    error.style.display = 'none';
+    checkboxError.style.display = 'none';
+    for (let i = 0; i < count; i += 1) {
+        preferences[i] = form[i].value;
     }
-  }
-  // return preferences
-  // console.log(preferences);
-  window.localStorage.setItem('preferences', JSON.stringify(preferences));
-  if (checkbox.checked === true) {
-    submitButton.href = '../html/index.html';
-  } else {
-    checkboxError.style.display = 'block';
-  }
+    // console.log(preferences)
+
+    if (preferences[0] === '') {
+        // console.log('empty');
+        error.classList.add(SHOW_CLS);
+        error.style.display = 'block';
+        return false;
+    }
+
+    for (let i = 0; i < count; i += 1) {
+        if (preferences[i] === '') {
+            for (let j = i + 1; j < count; j += 1) {
+                if (preferences[j] !== '') {
+                    // console.log('wrong order');
+                    error.style.display = 'block';
+                    return false;
+                }
+            }
+            // return preferences
+            // console.log(preferences);
+            window.localStorage.setItem('preferences', JSON.stringify(preferences));
+            if (checkbox.checked === true) {
+                submitButton.href = '../html/index.html';
+                return true;
+            }
+            checkboxError.style.display = 'block';
+        }
+    }
+    // return preferences
+    // console.log(preferences);
+    window.localStorage.setItem('preferences', JSON.stringify(preferences));
+    if (checkbox.checked === true) {
+        submitButton.href = '../html/index.html';
+    } else {
+        checkboxError.style.display = 'block';
+    }
 }
 
 // document.getElementById("clearButton").onclick = clearall()
 
 function getpref() {
-  preferences = JSON.parse(localStorage.getItem('preferences'));
-  // console.log(preferences)
-  for (let i = 0; i < count; i += 1) {
-    form[i].value = preferences[i];
-  }
-  // console.log(document.getElementById("flexCheckDefault").checked)
+    preferences = JSON.parse(localStorage.getItem('preferences'));
+    // console.log(preferences)
+    for (let i = 0; i < count; i += 1) {
+        form[i].value = preferences[i];
+    }
+    // console.log(document.getElementById("flexCheckDefault").checked)
+}
+
+/**
+ * Event delegation: a single form element accounts for its multiple children
+ * @param {Element} form form element
+ */
+function attachHandler(form) {
+    form.addEventListener("change", function onChangeHandler(event) {
+        const elm = event.target;
+        if (elm.className.includes("form-select")) {
+            check(elm);
+        }
+    });
 }
