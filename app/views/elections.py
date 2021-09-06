528564f9a9805950d9aa389eea905a18749f31a2
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-09-06 13:24:41 +0530

    Add details to intermediate modal Improve layout and details of candidate/election page

--- app/views/elections.py
+++ app/views/elections.py
@@ -72,6 +72,10 @@ def get_details_common_to_renders(election_id):
         "eligible_candidates": eligible_candidates,
         "ineligible_candidates": constituency_wise_ineligible_cands,
         "can_vote": can_vote,
+        "voter_consti_desc": constituency.voter_description,
+        "candi_consti_desc": constituency.candidate_description,
+        "consti_seats_count": constituency.open_positions,
+        "consti_compete_count": len(eligible_candidates),
     }
 
 
