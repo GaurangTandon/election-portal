40aeed6c8300b9233b42667ba3a39b2c2d3d88e6
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-09-06 13:29:07 +0530

    Add static faq route Make election title a hyperlink Add responsive classes on candidate blocks

--- app/views/static.py
+++ app/views/static.py
@@ -6,3 +6,8 @@ static_routes = Blueprint("static_routes", __name__)
 @static_routes.route("/security")
 def security():
     return "I'll work on this when I get time :sadcat:", 200
+
+
+@static_routes.route("/faq")
+def faq():
+    return "I'll work on this when I get time :sadcat:", 200
